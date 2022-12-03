95bdbe6dbb26ea894b73026b8b899957aad45522
Nijam <nijamudeen.96@gmail.com>
2022-12-03 14:17:49 +0800

    nijam - update auth

--- backend/auth.py
+++ backend/auth.py
@@ -7,11 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def login(username, password):
-    print("user: ", username, " password: ", password)
-    # userid = 1
-    # check database
     userid = db.login_user(username, password)
-    print("userid: ", userid)
     
     if int(userid):
         auth_token = jwt.encode({"username": username}, os.environ.get('JWT_SECRET'), algorithm="HS256")
@@ -25,16 +21,7 @@ def login(username, password):
             "user_id" : -1
         }
     return json.dumps(result)
-    
-    
-
 
 def is_valid_auth(msg):
-    print(msg)
-    print(type(msg))
     decoded_username = jwt.decode(msg, os.environ.get('JWT_SECRET'), algorithms="HS256")
-    # check if username in db
-
-    return json.dumps({
-        "username" : decoded_username['username']
-    })
+    return db.is_user(decoded_username)
